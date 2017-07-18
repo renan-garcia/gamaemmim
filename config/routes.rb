895f9d09155fec4a84ad8c987d15396b8600d854
Renan Garcia <email@renangarcia.me>
2017-07-17 21:49:46 -0300

    Adicionado Rake Last_name

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :leads, except: %i[edit update destroy]
+  resources :leads, except: %i[destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Lead.all.each do |lead|
+  full_name = lead.name.split(" ")
+  if(full_name.size <= 1)
+    lead.name = lead.name + " " + Faker::Name.last_name
+    lead.save
+  end
+end
